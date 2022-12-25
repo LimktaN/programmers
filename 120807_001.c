106d9428edb112089e0f6055ff13299b4aa9fbdd
LimktaN <qz2012@naver.com>
2022-12-25 14:58:37 +0900

    day2

---
+++ 120807_001.c
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    if(num1==num2)
+    {
+        return 1;
+    } else
+    {
+        return -1;
+    }
+}
